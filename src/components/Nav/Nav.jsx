a687a3b38eae47fed2dd243d5788321800dd838a
Divine Obeten <divineobeten9@gmail.com>
2023-08-31 12:12:01 +0100

    mobile nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import NavLogo from "../../assets/Fashionistar.svg";
 import "./Nav.css";
 
 const Nav = () => {
+  const [isOpen, setISOpen] = useState(false);
+
+  function handleClick() {
+    setISOpen(!isOpen);
+  }
+
   return (
     <div className="nav">
       <div className="nav__logo">
         <img src={NavLogo} alt="fashionistar Logo" />
       </div>
-      <div className="right__nav">
+
+      <div className="nav__burger" onClick={() => handleClick()}>
+        <div className={isOpen ? "burger__menu close" : "burger__menu"}></div>
+      </div>
+
+      <div className={isOpen ? "right__nav open" : "right__nav"}>
         <div className="nav__links">
           <ul>
             <li>About Us</li>
