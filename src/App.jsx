2ca473e01011006e05e7b39f5ea3ec739626c301
Divine Obeten <divineobeten9@gmail.com>
2023-09-01 10:27:13 +0100

    adding hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
 import Nav from "./components/Nav/Nav";
 import "./App.css";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import FaceModelling from "../../assets/Face Modelling.svg";
+import Commercial from "../../assets/Commercial.svg";
+import Runway from "../../assets/Runway.svg";
+import Editorial from "../../assets/Editorial.svg";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="Hero">
+      <div className="hero__text"></div>
+      <div className="hero__images"></div>
+    </div>
+  );
+};
+
+export default Hero;
