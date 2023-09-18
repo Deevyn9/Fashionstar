ed7b2c0ff9941e4d5f98b62786d5d3cf1bca7bba
Divine Obeten <divineobeten9@gmail.com>
2023-09-18 18:29:26 +0100

    hero input field

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import FaceModelling from "../../assets/Face Modelling.svg";
 import Commercial from "../../assets/Commercial.svg";
 import Runway from "../../assets/Runway.svg";
 import Editorial from "../../assets/Editorial.svg";
+
 import "./Hero.css";
 
 const Hero = () => {
   return (
     <div className="Hero">
-      <div className="hero__text"></div>
-      <div className="hero__images"></div>
+      <div className="hero__text">
+        <h1>Ready, Set, Pose!</h1>
+        <div className="hero__input">
+          <input type="text" placeholder="Want to know more?" />
+          <button>Text</button>
+        </div>
+      </div>
+      <div className="hero__images">
+        <div className="flex__image"></div>
+        <div className="flex__image"></div>
+        <div className="flex__image"></div>
+        <div className="flex__image"></div>
+      </div>
     </div>
   );
 };
