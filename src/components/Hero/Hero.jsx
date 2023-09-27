9eb38744b83c241b90889cc0802425118d08dd53
Divine Obeten <divineobeten9@gmail.com>
2023-09-27 11:03:19 +0100

    hover function working

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,58 @@ import Editorial from "../../assets/Editorial.svg";
 
 import "./Hero.css";
 
+const featuredItems = [
+  {
+    id: 1,
+    img: FaceModelling,
+    text: "Face Modelling",
+  },
+  {
+    id: 2,
+    img: Commercial,
+    text: "Commercial",
+  },
+  {
+    id: 3,
+    img: Runway,
+    text: "Runway",
+  },
+  {
+    id: 4,
+    img: Editorial,
+    text: "Editorial",
+  },
+];
+
 const Hero = () => {
+  const [hoveredItem, setHoveredItem] = useState(null);
+
+  function handleHover(item) {
+    if (hoveredItem !== item.id) {
+      setHoveredItem(item.id);
+    }
+  }
+
+  function removeHover() {}
+
+  const featured = featuredItems.map((item) => {
+    return (
+      <div
+        className={
+          hoveredItem === item.id ? "flex__image hovered" : "flex__image"
+        }
+        key={item.id}
+        onMouseEnter={() => handleHover(item)}
+        onMouseLeave={() => removeHover()}
+        style={{
+          backgroundColor: `${hoveredItem === item.id ? "red" : "transparent"}`,
+        }}
+      >
+        <img src={item.img} alt="" />
+      </div>
+    );
+  });
+
   return (
     <div className="Hero">
       <div className="hero__text">
@@ -17,10 +68,11 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero__images">
+        {featured}
+        {/* <div className="flex__image"></div>
         <div className="flex__image"></div>
         <div className="flex__image"></div>
-        <div className="flex__image"></div>
-        <div className="flex__image"></div>
+        <div className="flex__image"></div> */}
       </div>
     </div>
   );
